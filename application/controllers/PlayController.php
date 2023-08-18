e0c57c5937bcc2421648f58f6c0bfa5afb6f964b
Alexander A. Klimov <alexander.klimov@icinga.com>
2023-08-18 17:36:55 +0200

    Forbid playing before game start

--- application/controllers/PlayController.php
+++ application/controllers/PlayController.php
@@ -2,13 +2,24 @@
 
 namespace Icinga\Module\Fourcolors\Controllers;
 
+use Icinga\Module\Fourcolors\RedisAwareController;
+use Icinga\Security\SecurityException;
 use ipl\Html\Html;
 use ipl\Web\Compat\CompatController;
 
 class PlayController extends CompatController
 {
+    use RedisAwareController;
+
     public function indexAction(): void
     {
+        $game = $this->params->getRequired('game');
+        $state = $this->loadGame($this->getRedis(), $game);
+
+        if (! $state->started) {
+            throw new SecurityException($this->translate('Game not started yet: %s'), $game);
+        }
+
         $this->addContent(Html::tag('p'));
     }
 }
